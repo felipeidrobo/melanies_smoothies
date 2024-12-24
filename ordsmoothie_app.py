d7d7ddd9756e7d44a7330470058c9bc69745e8ab
Felipe Idrobo Avirama <140673618+felipeidrobo@users.noreply.github.com>
2024-12-24 03:03:31 -0500

    Update ordsmoothie_app.py

--- ordsmoothie_app.py
+++ ordsmoothie_app.py
@@ -1,9 +1,10 @@
 # Import python packages
 import streamlit as st
 import requests
+import pandas
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-import pandas
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -20,7 +21,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 # Convert the snowpark dataframe to a pandas dataframe
-pd_df=my_dataframe.to_pandas()
+pd_df = my_dataframe.to_pandas()
 # st.dataframe(pd_df)
 # st.stop()
 
