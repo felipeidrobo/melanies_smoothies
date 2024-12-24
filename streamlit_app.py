87f22ad7b5ca71800cf1fc6216642ca6144ec08b
Felipe Idrobo Avirama <140673618+felipeidrobo@users.noreply.github.com>
2024-12-24 01:21:37 -0500

    Update streamlit_app.py
    Connected changes needed bc of move from Sis TO sNIs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col,when_matched
 
 # Write directly to the app
@@ -8,8 +7,9 @@ st.title(":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
 st.write(
     """Orders that need to filled."""
 )
-
-session = get_active_session()
+cnx=st.connection('snowflake')
+session = cnx.session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")=='FALSE').collect()
 
 if my_dataframe:
