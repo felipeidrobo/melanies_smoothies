2c8e2e568afc0f37c9992b1978b1d5e1bfa7fdeb
Felipe Idrobo Avirama <140673618+felipeidrobo@users.noreply.github.com>
2024-12-24 02:57:21 -0500

    Update ordsmoothie_app.py

--- ordsmoothie_app.py
+++ ordsmoothie_app.py
@@ -5,7 +5,7 @@ from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.title(":cup_with_straw: Customize Your Smoothieee! :cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom Smoothie!"""
 )
